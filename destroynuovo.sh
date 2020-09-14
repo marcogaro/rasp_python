7d3b582026cc5f16cdf49ffec728c3a9bb5117a7
marcogaro <68847387+marcogaro@users.noreply.github.com>
2020-09-14 13:45:51 +0200

    Create destroynuovo.sh

---
+++ destroynuovo.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ "$#" -eq 1 ] ; then
+    	nome=$1	
+    	echo "nome container già inserito: "$nome ;
+elif [ "$#" -eq 0 ] ; then
+	echo "inserisci nome container: "
+	read -r b
+	#echo $b
+	nome=$b ;	
+else
+   	echo "errore" 
+   	exit
+fi
+
+
+
+
+pass=passt3.5
+
+
+echo "Destroying virtual rasp "$nome"!"
+
+
+
+
+sudo umount /gpio_mnt/"$nome"/sys/devices/platform/soc/3f200000.gpio
+sudo umount /gpio_mnt/"$nome"/sys/class/gpio
+
+sudo rm -rf $pass.$nome.py
+
+sudo umount /gpio_mnt/sys/devices/platform/soc/soc\:firmware/soc\:firmware\:expgpio/gpio/gpiochip504/
+sudo umount --force /gpio_mnt/"$nome"/sys/devices/platform/soc/3f200000.gpio
+sudo umount --force /gpio_mnt/"$nome"/sys/class/gpio
+
+sudo umount --force /gpio_mnt/sys/devices/platform/soc/soc\:firmware/soc\:firmware\:expgpio/gpio/gpiochip504/
+
+lxc config device remove "$nome" gpio disk 
+lxc config device remove "$nome" devices disk
+lxc config device remove "$nome" soc disk
+lxc config device remove "$nome" firmware disk
+lxc config device remove "$nome" bus disk
+
+sudo rm -rf /gpio_mnt/"$nome"
+#sudo rm -rf /gpio_mnt/
+sudo rm -rf /tmp/passthrough/
+
+lxc delete --force "$nome"
